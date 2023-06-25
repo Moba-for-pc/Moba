1341550cff6f6894e8655f436beef6b65b9dea60
aheront_aheront <k-maxim-k@mail.ru>
2023-06-26 01:07:07 +0700

    добавил префаб, который сразу перенаправляет на аутентификацию

---
+++ Assets/Scripts/UI/Test Scenes/Authentication/AuthRequestScene/FastAuthRequest.cs
@@ -0,0 +1,29 @@
+using Assets.Scripts.Authentication;
+using Assets.Scripts.Authentication.AuthenticationRequest;
+using UnityEngine;
+using Zenject;
+
+public class FastAuthRequest : MonoBehaviour
+{
+    private IAuthRequest _authRequest;
+    private IAuthenticator _authenticator;
+
+    [Inject]
+    public void Init(IAuthRequest authRequest, IAuthenticator authenticator)
+    {
+        _authRequest = authRequest;
+        _authenticator = authenticator;
+    }
+
+    private void Start()
+    {
+        if (!_authenticator.IsAuthenticated())
+        {
+            _authRequest.RedirectUserToAuthSceneAndBack();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
